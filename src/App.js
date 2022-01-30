ae3e0479e997d6438229c0793d5e02127f44b25a
garlife <garlife@gmail.com>
2022-01-30 23:41:52 +0300

    groomed

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function Car(props) {
   }
 
   return (
-    <div
-      className={classes.join(' ')}
-      onClick={props.onMark}
-      
-    >
+    <div className={classes.join(' ')} onClick={props.onMark}>
       <div style={{ width: '200px%' }}>
         <div className="card-img">
           <img
@@ -21,10 +17,7 @@ function Car(props) {
             alt={props.car.model}
           />
         </div>
-        <h3>
-          {props.car.mark}
-          {props.car.model}
-        </h3>
+        <h3>{props.car.mark} {props.car.model}</h3>
         <p>{props.car.price}&#8381;</p>
       </div>
     </div>
@@ -39,21 +32,21 @@ export default class App extends React.Component {
         {
           mark: 'bmw',
           model: 'x3',
-          price: '4670000',
+          price: '4 670 000',
           src: 'https://cdn.kodixauto.ru/media/image/5d8e13a659f5e20001c81a7d',
           marked: false,
         },
         {
           mark: 'bmw',
           model: '5',
-          price: '4120000',
+          price: '4 120 000',
           src: 'https://cdn.kodixauto.ru/media/image/5f3121205c3ba500016cf657',
           marked: false,
         },
         {
           mark: 'bmw',
           model: 'Z4 roadster',
-          price: '6797800',
+          price: '6 797 800',
           src: 'https://cdn.kodixauto.ru/media/image/5d8e161359f5e20001c81a8c',
           marked: false,
         },
@@ -84,14 +77,16 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <div style={{display: "flex", justifyContent: "center"}}>
-        <h1 >Выбери свое BMW!</h1>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <h1>Выбери свое BMW!</h1>
         </div>
-        <div className="list" style={{
-        display: 'flex',
-        justifyContent: 'space-around',
-      }}>
-          
+        <div
+          className="list"
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+          }}
+        >
           {this.renderCars()}
         </div>
       </div>
